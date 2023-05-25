d6ab5d9b0903e80158a0f6095d64cf21adf24b95
Shakhzodbek <55225173+shakhzodbek1997@users.noreply.github.com>
2023-05-25 19:00:10 +0200

    Initial App and Database set up using custom traversyMVC freamwork

---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_NAME', 'shareposts');
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://localhost/shareposts');
+  // Site Name
+  define('SITENAME', 'SharePosts');
