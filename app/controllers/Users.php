58caa1172784b72896421d923eb9890d0490316d
Shakhzodbek <55225173+shakhzodbek1997@users.noreply.github.com>
2023-05-26 11:46:44 +0200

    Custom Flash Messaging and Login are Done

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -64,6 +64,7 @@
 
           // Register User 
           if($this->userModel->register($data)){
+            flash('register_success', 'You are registered and can Login In');
             redirect('users/login');
           }else{
             die('Something Went Wrong');
